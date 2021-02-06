c3022ebe1da1ba7e36be1463c8c6fb2081f887ac
Igorstoso <igoraugustomarchi@gmail.com>
2021-02-06 12:21:47 -0300

    controller de eventos adicionado

---
+++ DevEvents.API/Controllers/EventosController.cs
@@ -0,0 +1,48 @@
+﻿using DevEvents.API.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEvents.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            var eventos = new List<Evento>
+            {
+                new Evento {Titulo = ".NET CORE", Descricao = "Venha prender tudo sober C#"},
+                new Evento {Titulo = "Mobile", Descricao = "Venha aprender Mobile"}
+            };
+            return Ok(eventos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterEvento(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody] Evento evento)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            return NoContent();
+        }
+    }
+}
