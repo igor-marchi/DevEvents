69375db98dab763684f618fa1402db680a1217cd
Igorstoso <igoraugustomarchi@gmail.com>
2021-02-06 12:26:51 -0300

    adicionado controller de categorias (GET, POST)

---
+++ DevEvents.API/Controllers/CategoriasController.cs
@@ -0,0 +1,28 @@
+﻿using DevEvents.API.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevEvents.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObterTodas()
+        {
+            var categorais = new List<Categoria>
+            {
+                new Categoria { Descricao = "Mobile"},
+                new Categoria { Descricao = ".NET CORE"},
+                new Categoria { Descricao = "Machine Learning"},
+            };
+            return Ok(categorais);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody] Categoria categoria)
+        {
+            return Ok();
+        }
+    }
+}
--- DevEvents.API/Controllers/EventosController.cs
+++ DevEvents.API/Controllers/EventosController.cs
@@ -1,9 +1,6 @@
 ﻿using DevEvents.API.Entidades;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevEvents.API.Controllers
 {
